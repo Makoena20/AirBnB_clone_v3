8fe4a219b21f09555452e86498e5cd90a3f2d4ba
Makoena20 <koenaletseku@gmail.com>
2024-06-10 09:06:29 +0000

    api

---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines the routes for the API status."""
+from flask import jsonify
+from api.v1.views import app_views
+
+@app_views.route('/status', methods=['GET'])
+def get_status():
+    """Returns the status of the API."""
+    return jsonify({"status": "OK"})
+
